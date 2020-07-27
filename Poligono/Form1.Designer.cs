4abe71aa71e2fb895b4bd02bff9557b295d58652
giribroba <giribroba@gmail.com>
2020-07-27 00:34:30 -0300

    pixel e linha

---
+++ Poligono/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Poligono
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Picture = new System.Windows.Forms.PictureBox();
+            this.Button = new System.Windows.Forms.Button();
+            this.Xibx = new System.Windows.Forms.TextBox();
+            this.Yibx = new System.Windows.Forms.TextBox();
+            this.Xfbx = new System.Windows.Forms.TextBox();
+            this.Yfbx = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Picture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Picture
+            // 
+            this.Picture.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.Picture.Location = new System.Drawing.Point(12, 12);
+            this.Picture.Name = "Picture";
+            this.Picture.Size = new System.Drawing.Size(776, 296);
+            this.Picture.TabIndex = 0;
+            this.Picture.TabStop = false;
+            // 
+            // Button
+            // 
+            this.Button.Location = new System.Drawing.Point(618, 353);
+            this.Button.Name = "Button";
+            this.Button.Size = new System.Drawing.Size(138, 36);
+            this.Button.TabIndex = 1;
+            this.Button.Text = "button1";
+            this.Button.UseVisualStyleBackColor = true;
+            this.Button.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // Xibx
+            // 
+            this.Xibx.Location = new System.Drawing.Point(26, 337);
+            this.Xibx.Name = "Xibx";
+            this.Xibx.Size = new System.Drawing.Size(105, 20);
+            this.Xibx.TabIndex = 2;
+            // 
+            // Yibx
+            // 
+            this.Yibx.Location = new System.Drawing.Point(26, 369);
+            this.Yibx.Name = "Yibx";
+            this.Yibx.Size = new System.Drawing.Size(105, 20);
+            this.Yibx.TabIndex = 3;
+            // 
+            // Xfbx
+            // 
+            this.Xfbx.Location = new System.Drawing.Point(156, 337);
+            this.Xfbx.Name = "Xfbx";
+            this.Xfbx.Size = new System.Drawing.Size(105, 20);
+            this.Xfbx.TabIndex = 4;
+            // 
+            // Yfbx
+            // 
+            this.Yfbx.Location = new System.Drawing.Point(156, 369);
+            this.Yfbx.Name = "Yfbx";
+            this.Yfbx.Size = new System.Drawing.Size(105, 20);
+            this.Yfbx.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Yfbx);
+            this.Controls.Add(this.Xfbx);
+            this.Controls.Add(this.Yibx);
+            this.Controls.Add(this.Xibx);
+            this.Controls.Add(this.Button);
+            this.Controls.Add(this.Picture);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Picture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Picture;
+        private System.Windows.Forms.Button Button;
+        private System.Windows.Forms.TextBox Xibx;
+        private System.Windows.Forms.TextBox Yibx;
+        private System.Windows.Forms.TextBox Xfbx;
+        private System.Windows.Forms.TextBox Yfbx;
+    }
+}
+
---
+++ Poligono/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Poligono
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            Bitmap papel = new Bitmap(Picture.Width, Picture.Height);
+            Graphics desenhador = Graphics.FromImage(papel);
+
+            Linha(desenhador);
+
+            Picture.BackgroundImage = papel;
+        }
+
+        private void Linha(Graphics desenhador)
+        {
+            var Xi = int.Parse(Xibx.Text);
+            var Xf = int.Parse(Xfbx.Text);
+            var Yi = int.Parse(Yibx.Text);
+            var Yf = int.Parse(Yfbx.Text);
+
+            float dx = Math.Abs(Xi - Xf);
+            float dy = Math.Abs(Yi - Yf);
+
+            var steps = (dx > dy ? dx : dy);
+
+            float xInc = (float)(dx/steps);
+            float yInc = (float)(dy/steps);
+
+            float xTemp = 0;
+            float yTemp = 0;
+
+            var eX = 0;
+            var eY = 0;
+
+            for (int i = 0; i < steps; i++)
+            {
+                if (Math.Abs(Xi + xTemp - Math.Floor(Xi + xTemp)) != 0.5f)
+                {
+                    if (Math.Abs(Yi + yTemp - Math.Floor(Yi + yTemp)) != 0.5f)
+                    {
+                        Px((int) Math.Round(Xi + xTemp),(int) Math.Round(Xf + yTemp), desenhador);
+                    }
+                    else
+                    {
+                        Px((int)Math.Round(Xi + xTemp), (int)Math.Round(Xf + yTemp + (eY % 2 == 0 ? -0.5f : +0.5f)), desenhador);
+                        eY++;
+                    }
+                }
+                else
+                {
+                    if (Math.Abs(Yi + yTemp - Math.Floor(Yi + yTemp)) != 0.5f)
+                    {
+                        Px((int)Math.Round(Xi + xTemp +(eX % 2 == 0 ? -0.5f : +0.5f)), (int)Math.Round(Xf + yTemp), desenhador);
+                    }
+                    else
+                    {
+                        Px((int)Math.Round(Xi + xTemp + (eX % 2 == 0 ? -0.5f : +0.5f)), (int)Math.Round(Xf + yTemp + (eY % 2 == 0 ? -0.5f : +0.5f)), desenhador);
+                        eY++;
+                    }
+                    eX++;
+                }
+
+                xTemp += xInc;
+                yTemp += yInc;
+            }
+        }
+
+        private void Px(int x, int y, Graphics desenhador)
+        {
+            var caneta = new Pen(Color.Black, 1);
+            var pix = new Rectangle(Picture.Width/2 + x, Picture.Height/2 + y, 1, 1);
+
+            desenhador.DrawRectangle(caneta, pix);
+        }
+
+
+    }
+}
